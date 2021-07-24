4b9b313bb674ad6146f01d92a8a189e6dcd6e237
huangyi <245807235@qq.com>
2021-07-24 22:00:50 +0800

    add

---
+++ nav.config.ts
@@ -0,0 +1,58 @@
+import { IConfig } from './src/types'
+
+const c: IConfig = {
+  // [必填], 请填写您的仓库地址
+  gitRepoUrl: 'https://github.com/cbxbw/nav',
+
+  // 部署分支
+  branch: 'main',
+
+  // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
+  hashMode: true,
+
+  // 是否显示右上角的 Github 图标
+  showGithub: false,
+
+  // 您的网站地址，有利于SEO
+  homeUrl: '',
+
+  // 网站标题
+  title: '自用导航',
+
+  // 网站描述
+  description: '自用导航',
+
+  // 网站关键字
+  keywords: '自用',
+
+  // 默认主题: Light | Sim | Side | App | Shortcut
+  theme: 'Light',
+
+  // 网站底部内容, 版权信息、备案号, 可以是 HTML
+  footerContent: `
+    <div style="font-weight: bold;">共收录\${total}个网站</div>
+  `,
+
+  // 阿里巴巴矢量图库 https://www.iconfont.cn/
+  // 用在 Side 主题一级、二级菜单图标展示
+  iconfontUrl: '//at.alicdn.com/t/font_2522843_wl70o31sy6.js',
+
+  // 百度统计地址
+  // https://tongji.baidu.com/web/welcome/login
+  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+
+  // CNZZ 统计地址
+  // https://www.cnzz.com/o_index.php
+  cnzzStatisticsUrl: '',
+
+  // Sim 主题配置
+  simThemeConfig: {
+    // 海报图
+    posterImageUrls: [
+      'https://raw.sevencdn.com/xjh22222228/nav/image/sim-wallpaper.jpg'
+    ],
+    description: '收录<b>${total}</b>个网站'
+  }
+}
+
+export default c
